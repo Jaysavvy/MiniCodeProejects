1a1c2299e0f63fc6ea7bed9d7fe54e80f608ab02
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-02-01 22:08:39 -0500

    CRUD App Builder Practice

---
+++ rap-names-express-main 2/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const PORT = 2020;
+
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs')
+
+
+// this line of code does a response to from the client to get ejs file 
+app.get('/', (request, respnse) =>{ 
+    
+    respnse.send(__dirname + '/index.ejs')
+})
+
+// this line of code does a post of from ejs to the client 
+app.post('/', (request, respnse) =>{ 
+    
+    respnse.send(__dirname + '/index.ejs')
+})
+
+// this line of code 
+app.put('/', (request, respnse) =>{ 
+    
+    respnse.send(__dirname + '/index.ejs')
+})
+
+app.delete('/', (request, respnse) =>{ 
+    
+    respnse.send(__dirname + '/index.ejs')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
