3db7e959a161d4a1fe52a7b777800beee7d4df9a
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-02-04 12:02:38 -0500

    added a event listener

--- rap-names-express-main 2/public/js/main.js
+++ rap-names-express-main 2/public/js/main.js
@@ -1,2 +1,6 @@
 const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteRapper)
+})
