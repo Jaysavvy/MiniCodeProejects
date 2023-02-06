9bcd8f92d3f344eb4b95f45a5a99640bf51c6021
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-02-06 15:54:23 -0500

    To do list started

---
+++ React Todo List/react-task-tracker/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Componets/Header'
+
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <Header></Header>
+    </div>
+  
+  )
+}
+
+
+export default App;
